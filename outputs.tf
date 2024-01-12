457ff5941fcc27007219ad262621c59c1717eccc
Russ Parsloe <russparsloe@users.noreply.github.com>
2024-01-12 14:43:05 +0000

    Modify cluster-admin outputs

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,12 @@ output "public_endpoint_url" {
   description = "The public URL for the Vault cluster"
 }
 
+output "cluster_admin_username" {
+  value       = "cluster-admin"
+  description = "The username for the cluster-admin account"
+}
+
 output "cluster_admin_password" {
-  value       = random_password.userpass_cluster_admin_password.result
-  sensitive   = true
+  value       = nonsensitive(random_password.userpass_cluster_admin_password.result)
   description = "The password for the cluster-admin account"
 }
